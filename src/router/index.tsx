34e7015691e6aae582d23b1177094b6c24763706
jiangnan <764234162@qq.com>
2025-03-19 17:25:41 +0800

    fix:memoRouter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import Routes from "./routers";
 import { Suspense } from "react";
 import Loading from "../components/Loading";
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <MemoryRouter>
       <Suspense fallback={<Loading />}>
         <Routes />
       </Suspense>
-    </BrowserRouter>
+    </MemoryRouter>
   );
 }
