7d231e9eb5cefe335e1d6e950d738978076920f9
jiangnan <764234162@qq.com>
2025-03-19 17:45:41 +0800

    router basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,14 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routes from "./routers";
 import { Suspense } from "react";
 import Loading from "../components/Loading";
 
 export default function Router() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/learning">
       <Suspense fallback={<Loading />}>
         <Routes />
       </Suspense>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
