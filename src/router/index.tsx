de4318ea05d6b71004732017b27629876ba01c6b
jiangnan <764234162@qq.com>
2025-03-19 17:28:56 +0800

    hashrouter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,14 @@
-import { MemoryRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Routes from "./routers";
 import { Suspense } from "react";
 import Loading from "../components/Loading";
 
 export default function Router() {
   return (
-    <MemoryRouter>
+    <HashRouter>
       <Suspense fallback={<Loading />}>
         <Routes />
       </Suspense>
-    </MemoryRouter>
+    </HashRouter>
   );
 }
