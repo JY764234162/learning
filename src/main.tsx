a62d5d703acee90fed803faf462c17f1489d3c53
jiangnan <764234162@qq.com>
2025-04-17 18:15:08 +0800

    fix:贝塞尔曲线 tabs

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import eruda from "eruda";
-
-eruda.init();
 
 const rootElement = document.getElementById("root");
 
---
+++ src/pages/bezierTabs/index.tsx
@@ -0,0 +1,52 @@
+import React, { useRef, useState } from "react";
+
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="tabs-container">
+      <Tabs></Tabs>
+    </div>
+  );
+}
+
+const Tabs = () => {
+  const tabs = [
+    { key: 1, name: "tab1" },
+    { key: 2, name: "tab2" },
+  ];
+  const [activeTab, setActiveTab] = useState<string>("tab1");
+  return (
+    <>
+      <div className="custom-tabs">
+        {tabs.map((item) => {
+          return (
+            <div
+              className={activeTab === item.name ? "active tab" : "tab"}
+              onClick={() => {
+                setActiveTab(item.name);
+              }}
+            >
+              {item.name}
+            </div>
+          );
+        })}
+        <div className="bezier-container">
+          <div
+            className="bezier"
+            style={{
+              clipPath:
+                activeTab === "tab1"
+                  ? 'path("M0 0 C30 0, 0 50,30 50 L 30 50 L0 50 z")'
+                  : 'path("M0 50 C30 50, 0 0,30 0 L 30 0 L30 50 z")',
+            }}
+          ></div>
+        </div>
+      </div>
+      <div className="content">
+        {activeTab === "tab1" && <div>tab1</div>}
+        {activeTab === "tab2" && <div>tab2</div>}
+      </div>
+    </>
+  );
+};
