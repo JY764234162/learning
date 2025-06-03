39f408ad20fa2ec3b66b3fe1941d3eb0ee5e29b6
jiangnan <764234162@qq.com>
2025-06-03 16:42:14 +0800

    feat:性能监控

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import { ConfigProvider } from "antd";
 import Router from "./router";
 import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+import { initializePerformanceMonitoring } from "./stores/performanceStore";
+
 export default function App() {
+  useEffect(() => {
+    const cleanup = initializePerformanceMonitoring();
+    return cleanup;
+  }, []);
+
   return (
     <ConfigProvider>
       <Router />
