11923da908f3887d34690e26309f1fb65dbf2766
jiangnan <764234162@qq.com>
2025-06-06 17:01:08 +0800

    fix:typescript 泛型

--- packages/frontend/src/types/interface.ts
+++ packages/frontend/src/types/interface.ts
@@ -157,3 +157,85 @@ type PickRequired<T extends object> = {
     ? K
     : never]: T[K];
 };
+
+/**
+ * 从两个对象类型中提取出同时存在的属性
+ * @template T - 第一个对象类型
+ * @template U - 第二个对象类型
+ * @example
+ * type Output = Intersection<{ key1: number }, { key1: string }>; // {}  类型不一致
+ * type Output2 = Intersection<{ key1: number }, { key1: number; key2: number }>; // { key1: number }
+ */
+type Intersection<T extends object, U extends object> = {
+  [K in keyof T & keyof U as K extends IfEquals<
+    { [P in K]: T[P] },
+    { [P in K]: U[P] },
+    K
+  >
+    ? K
+    : never]: T[K];
+};
+
+/**
+ * 合并两个对象类型，如果存在相同的属性，则以第二个对象的属性为准
+ * @template T - 第一个对象类型
+ * @template U - 第二个对象类型
+ * @example
+ * type Output = OverWrite<{ key1: number }, { key1: string; other: "other" }>; // { key1: string }
+ */
+type OverWrite<T extends object, U extends object> = {
+  [K in keyof T & keyof U]: U[K];
+};
+
+/**
+ * 将对象类型中的指定属性设置为可选属性
+ * @template T - 输入的对象类型
+ * @template U - 要设置为可选属性的键
+ * @example
+ * type Input = { a: number, b: string };
+ * type Output = SetOptional<Input, 'a'>; // { a?: number, b: string }
+ */
+type SetOptional<T extends object, U extends keyof T> = {
+  [K in U]?: T[K];
+} & Omit<T, U>;
+
+/**
+ * 根据类型选择对象中的属性
+ * @template T - 输入的对象类型
+ * @template U - 要选择的属性类型
+ * @example
+ * type Input = { a: string, b: number, c: boolean };
+ * type Output = PickByType<Input, boolean>; // { c: boolean }
+ */
+type PickByType<T extends object, U extends T[keyof T]> = {
+  [K in keyof T as IfEquals<T[K], U, K, never>]: T[K];
+};
+
+/**
+ * 添加一个参数到函数类型中
+ * @template F - 输入的函数类型
+ * @template A - 添加的参数类型
+ * @example
+ * type Fn = (a: number, b: string) => number | string;
+ * type Output = AppendArgument<Fn, boolean>; // (x: boolean, a: number, b: string) => number | string
+ */
+type AppendArgument<F extends (...args: any[]) => any, A> = (
+  x: A,
+  ...args: Parameters<F>
+) => ReturnType<F>;
+
+/**
+ * 扁平化数组
+ * @template T - 输入的数组类型
+ * @example
+ * type Input = ["a", ["b", ["c", ["e"]]], ["d"]];
+ * type Output = NaiveFlat<Input>; // "a" | "b" | "c" | "e" | "d"
+ * type Input2 = [1, [2, [3, [4, 1]]]];
+ * type Output2 = NaiveFlat<Input2>; // 1 | 2 | 3 | 4
+ */
+
+type DeepFlat<T> = T extends [infer First, ...infer Rest]
+  ? DeepFlat<First> | DeepFlat<Rest>
+  : T extends any[]
+  ? DeepFlat<T[number]>
+  : T;
