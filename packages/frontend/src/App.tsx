5ba259b43ab19ffde69d08dcb26cee325de10165
jiangnan <764234162@qq.com>
2025-06-10 15:52:27 +0800

    fix:删除性能监视页面

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,14 +3,8 @@ import Router from "./router";
 import "./App.css";
 import axios from "axios";
 import { useEffect } from "react";
-import { initializePerformanceMonitoring } from "./stores/performanceStore";
 
 export default function App() {
-  useEffect(() => {
-    const cleanup = initializePerformanceMonitoring();
-    return cleanup;
-  }, []);
-
   return (
     <ConfigProvider>
       <Router />
