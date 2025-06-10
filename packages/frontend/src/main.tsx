92908ed82bb64ed33167f65e5e9bf38648e11ae6
jiangnan <764234162@qq.com>
2025-06-10 15:50:30 +0800

    fix:带样式控制台打印

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -21,3 +21,9 @@ if (import.meta.hot) {
     console.log("updated: count is now ", newModule);
   });
 }
+
+console.log(
+  `%c${import.meta.env.VITE_APP_TITLE}%c江一`,
+  "color: white; font-size: 12px; font-weight: bold; background: red; padding: 3px 5px;",
+  "color: white; font-size: 12px; font-weight: bold; background: black; padding:3px 5px;"
+);
