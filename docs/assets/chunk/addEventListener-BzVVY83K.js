88f61d0eeff15e3577991a89b82eca6abefa1144
jiangnan <764234162@qq.com>
2025-11-11 17:20:05 +0800

    fix:build

--- docs/assets/chunk/addEventListener-BzVVY83K.js
+++
@@ -1,1 +0,0 @@
-import{by as r}from"../../main-C6dAISBj.js";function u(e,n,d,i){var o=r.unstable_batchedUpdates?function(v){r.unstable_batchedUpdates(d,v)}:d;return e!=null&&e.addEventListener&&e.addEventListener(n,o,i),{remove:function(){e!=null&&e.removeEventListener&&e.removeEventListener(n,o,i)}}}export{u as a};
--- docs/assets/chunk/index-DmrvXQK9.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"../../main-C6dAISBj.js";function s(){return e.jsx("div",{className:"w-[500px]",children:"123"})}export{s as default};
